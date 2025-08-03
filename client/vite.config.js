4d7a0aaf4fd7590ee01d048b33cddbf564c32058
Mayank Pandey <121632123+Mayank-Pandey1@users.noreply.github.com>
2025-08-03 17:19:21 +0000

    preview host in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 4173, // or your port
-    allowedHosts: ['webchat-frontend-m.onrender.com'], // ← Add your Render domain here
+    allowedHosts: ["webchat-frontend-m.onrender.com"], // ← Add your Render domain here
   },
   plugins: [react(), tailwindcss()],
 })
