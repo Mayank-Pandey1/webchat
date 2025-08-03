8c4a07d3269dc627ee4c48eca82540403b760201
Mayank Pandey <121632123+Mayank-Pandey1@users.noreply.github.com>
2025-08-03 15:06:31 +0000

    vite config edit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",   // Required for Render
+    port: 5173,         // Or any port, Render will detect
+  },
   plugins: [react(), tailwindcss()],
 })
